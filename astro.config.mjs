1b9b9374e66ce5efefde063a8fdead081e661684
Chris Levine <chris.levine@centricconsulting.com>
2023-06-01 19:03:50 -0500

    First push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,39 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import NetlifyCMS from "astro-netlify-cms";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
-	site: `https://astro.build`,
+  integrations: [
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: "git-gateway",
+          branch: "main",
+        },
+        collections: [
+          // Define a blog post collection
+          {
+            name: "pages",
+            label: "Pages",
+            folder: "src/content/pages/",
+            create: true,
+            delete: true,
+            fields: [
+              { name: "title", widget: "string", label: "Post Title" },
+              { name: "body", widget: "markdown", label: "Post Body" },
+            ],
+          },
+        ],
+        previewStyles: [
+          // Path to a local CSS file, relative to your project’s root directory
+          'src/styles/index.css',
+          'src/styles/theme.css',
+        ],
+      },
+    }),
+  ],
+  site: `https://astro.build`,
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 export const SITE = {
-	title: 'Documentation',
+	title: 'Design System Documentation',
 	description: 'Your website description.',
 	defaultLanguage: 'en-us',
 } as const;
@@ -14,33 +14,3 @@ export const OPEN_GRAPH = {
 	twitter: 'astrodotbuild',
 };
 
-export const KNOWN_LANGUAGES = {
-	English: 'en',
-} as const;
-export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
-
-export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
-
-export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
-
-// See "Algolia" section of the README for more information.
-export const ALGOLIA = {
-	indexName: 'XXXXXXXXXX',
-	appId: 'XXXXXXXXXX',
-	apiKey: 'XXXXXXXXXX',
-};
-
-export type Sidebar = Record<
-	(typeof KNOWN_LANGUAGE_CODES)[number],
-	Record<string, { text: string; link: string }[]>
->;
-export const SIDEBAR: Sidebar = {
-	en: {
-		'Section Header': [
-			{ text: 'Introduction', link: 'en/introduction' },
-			{ text: 'Page 2', link: 'en/page-2' },
-			{ text: 'Page 3', link: 'en/page-3' },
-		],
-		'Another Section': [{ text: 'Page 4', link: 'en/page-4' }],
-	},
-};
